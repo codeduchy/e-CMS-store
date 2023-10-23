4156def8462f69fda29de904c98943825f37a97a
codeduchy <codeduchy@gmail.com>
2023-10-23 16:06:42 +0100

    update 1

---
+++ components/navbar-actions.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Button from "./button";
+
+const NavbarActions = () => {
+  return (
+    <div className="ml-auto flex items-center gap-x-4">
+      <Button></Button>
+    </div>
+  );
+};
+
+export default NavbarActions;
