e5a6a291b67dc8a8599be59b0b9048ff46cdaa76
codeduchy <codeduchy@gmail.com>
2023-10-23 17:44:54 +0100

    environment setup & fetured projects

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,3 +1,23 @@
-export default function HomePage() {
-  return <div>Home</div>;
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import Container from "@/components/container";
+import ProductList from "@/components/product-list";
+
+export const revalidate = 0;
+
+export default async function HomePage() {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("4094de4d-904b-44da-b9d2-c75b568d28ff");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
 }
