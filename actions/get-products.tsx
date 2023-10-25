dc261c137b12928e13ba3524cd3f167f5f2075b9
codeduchy <codeduchy@gmail.com>
2023-10-25 12:07:30 +0100

    individual product screen

--- actions/get-products.tsx
+++ actions/get-products.tsx
@@ -19,6 +19,7 @@ const getProducts = async (query: Query): Promise<Product[]> => {
       isFeatured: query.isFeatured,
     },
   });
+
   const res = await fetch(url);
 
   return res.json();
--- types.ts
+++ types.ts
@@ -22,7 +22,7 @@ export interface Product {
 }
 
 export interface Image {
-  is: string;
+  id: string;
   url: string;
 }
 
